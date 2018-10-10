1dd8c434ca1b25eade64d1cbd55c9a2f11ad35ee
vieruuuu <mixerandr3w001@gmail.com>
2018-10-10 22:31:17 +0300

    added conditional expressions and removed indenting, now indenting is made using crystal tool format

--- example/compiled.cr
+++ example/compiled.cr
@@ -1,3 +1,13 @@
-x = read_line()
-puts(x.downcase())
+def start(*arguments)
+  ->{
+    t = 0
+    while (t < 101)
+      e = t.to_s
+      ((t % 2) == 0) ? puts((e + " este par")) : puts((e + " este impar"))
 
+      t += 1
+    end
+  }.call
+end
+
+start()
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ global.snippets = [];
 const babelParser = require("@babel/parser");
 const fs = require("fs");
 const { join } = require("path");
+const generate = require("./lib/finalCodeGenerator");
 const file = fs.readFileSync(process.argv[2]);
 const parsed = babelParser.parse(file.toString()).program.body;
-const generate = require("./lib/finalCodeGenerator");
 let code = generate(parsed);
 
 console.log(code);
